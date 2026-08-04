e44fabfb7868e911ccbf64b49584b382f58a97f8
Agentic-JJ-Web3 <njei.private@gmail.com>
2026-08-05 00:56:50 +0100

    Admin App which is rendered

---
+++ frontend/src/admin/AdminApp.jsx
@@ -0,0 +1,30 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AdminAuthProvider, RequireAdminAuth } from './AdminAuthContext';
+import AdminLayout from '../components/AdminLayout';
+import AdminLoginPage from '../pages/admin/AdminLoginPage';
+import AdminQueuePage from '../pages/admin/AdminQueuePage';
+import AdminOrderDetailPage from '../pages/admin/AdminOrderDetailPage';
+import AdminSettingsPage from '../pages/admin/AdminSettingsPage';
+
+export default function AdminApp() {
+  return (
+    <AdminAuthProvider>
+      <Routes>
+        <Route path="login" element={<AdminLoginPage />} />
+        <Route
+          element={
+            <RequireAdminAuth>
+              <AdminLayout />
+            </RequireAdminAuth>
+          }
+        >
+          <Route path="queue" element={<AdminQueuePage />} />
+          <Route path="orders/:reference" element={<AdminOrderDetailPage />} />
+          <Route path="settings" element={<AdminSettingsPage />} />
+          <Route index element={<Navigate to="queue" replace />} />
+        </Route>
+        <Route path="*" element={<Navigate to="queue" replace />} />
+      </Routes>
+    </AdminAuthProvider>
+  );
+}
